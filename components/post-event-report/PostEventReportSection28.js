1c012680432e782b95ba5fe084bcf59e62f800e1
leoncan122 <leon.can122@gmail.com>
2022-08-23 12:05:28 +0200

    last survey sections

--- components/post-event-report/PostEventReportSection28.js
+++ components/post-event-report/PostEventReportSection28.js
@@ -1,8 +1,88 @@
-import React from 'react'
+import React from 'react';
+import DemographicSurveySection from './DemographicSurveySection';
 
-const PostEventReportSection28 = () => {
+const PostEventReportSection28 = ({setEventForm}) => {
   return (
-    <div>PostEventReportSection28</div>
+    <div className="grid grid-cols-1 gap-7">
+    <h2 className="font-black">Cholesterol Testing</h2>
+    <label className="grid grid-cols-4">
+      <p>Cholesterol Testing Agency</p>
+      <input
+        className="border-black ml-2"
+        type="text"
+        placeholder="Please specify"
+        name="cholesterolTestingAgency"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    <label className="grid grid-cols-4">
+      <p>Total tested for Cholesterol</p>
+      <input
+        className="border-black ml-2"
+        type="number"
+        placeholder="type a number"
+        name="cholesterolTestedTotal"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    <label className="grid grid-cols-4">
+      <p>Total # of Reactive Results</p>
+      <input
+        className="border-black ml-2"
+        type="number"
+        placeholder="type a number"
+        name="cholesterolReactiveResults"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    <label className="grid grid-cols-4">
+      <p>Total # Linked to Care</p>
+      <input
+        className="border-black ml-2"
+        type="number"
+        placeholder="type a number"
+        name="cholesterolLinkedToCare"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    <label className="grid grid-cols-4">
+      <p>Which services were they referred to?</p>
+      <input
+        className="border-black ml-2"
+        type="text"
+        placeholder="type a number"
+        name="cholesterolServicesReferredTo"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    
+    <DemographicSurveySection setEventForm={setEventForm} typeOfTest="cholesterol"/>
+  </div>
   )
 }
 
