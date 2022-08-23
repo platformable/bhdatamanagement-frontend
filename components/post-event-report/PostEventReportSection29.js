6d2a514113c7fbfb6812dfdc6ec166572ae79494
Alexei Garban <garban.valdeon@gmail.com>
2022-08-23 11:59:03 +0200

    new section

--- components/post-event-report/PostEventReportSection29.js
+++ components/post-event-report/PostEventReportSection29.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import DemographicSurveySection from './DemographicSurveySection'
 
-const PostEventReportSection29 = () => {
+const PostEventReportSection29 = ({setEventForm}) => {
   return (
-    <div>PostEventReportSection29</div>
+    <div className="grid grid-cols-1 gap-7 my-10">
+    <h2 className="font-black">Covid 19 Testing</h2>
+    <label className="grid grid-cols-4">
+      <p>Covid Testing Agency</p>
+      <input
+        className="border-black ml-2"
+        type="text"
+        placeholder="Please specify"
+        name="covidTestingAgency"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    <label className="grid grid-cols-4">
+      <p>Total tested for COVID-19</p>
+      <input
+        className="border-black ml-2"
+        type="number"
+        placeholder="type a number"
+        name="covidTestedTotal"
+        onChange={(e) =>
+          setEventForm((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+          }))
+        }
+      />
+    </label>
+    
+  </div>
   )
 }
 
