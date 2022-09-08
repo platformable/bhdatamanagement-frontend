d85af56b2ae1d5f1b8d5010b392abc8932e0e986
Alexei Garban <garban.valdeon@gmail.com>
2022-09-08 13:03:43 +0200

    change input

--- components/events/Section2.js
+++ components/events/Section2.js
@@ -12,7 +12,7 @@ const Section2 = ({eventForm, setEventForm,event}) => {
         <div className='p-5  rounded'>
             <h1 className='mb-3 font-black'>What is the name of the event? Please only use letters and numbers in the name.</h1>
             <input type="text" 
-            className="w-full p-2 border rounded" 
+            className="w-134 p-2 border rounded" 
             placeholder='Type the name of the event here' 
             onChange={handleForm}
             defaultValue={event?event.eventname:""}
