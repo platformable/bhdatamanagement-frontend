8890161e91e7e4fbf8dc773d1f814ab71a29cb9b
leoncan122 <leon.can122@gmail.com>
2022-09-08 18:51:49 +0200

    updates

--- pages/events/index.js
+++ pages/events/index.js
@@ -10,30 +10,30 @@ const EventsIndex = ({events}) => {
   const sortedEventsByDate  = events.sort((a, b) => new Date(b.eventdate) - new Date(a.eventdate))
   return (
     <Layout showStatusHeader={true}>
-      <PageTopHeading pageTitle={"Manage existing events"} dashboardBtn={true} backBtn={false} />
+      <PageTopHeading pageTitle={"Manage existing events"} dashboardBtn={true} backBtn={true} />
 
-      <div className="container mx-auto md:px-0 px-5 mb-10 pb-10 rounded-lg border">
+      <div className="container mx-auto md:px-0 px-7 mb-10 pb-10 rounded-lg ">
          {/* TABLE HEAD  */}
-         <div className="existing-events-head-table rounded-t-lg py-3 px-1 bg-black text-white">
-                <p className="lg:text-lg flex items-center justify-center">Program</p>
-                <p className="lg:text-lg flex items-center ">Event name</p>
-                <p className="lg:text-lg flex items-center ">Event Date</p>
-                <p className="lg:text-lg flex items-center justify-center text-center">Edit</p>
-                <p className="lg:text-lg flex items-center justify-center text-center">Go to participant surveys</p>
-                <p className="lg:text-lg flex items-center justify-center text-center">Upload event documents</p>
-                <p className="lg:text-lg flex items-center justify-center text-center">Complete post-event survey</p>
+         <div className="existing-events-head-table rounded-t-lg py-3 px-7 bg-black text-white">
+                <p className="lg:text-xl font-bold flex items-center ">Program</p>
+                <p className="lg:text-xl font-bold flex items-center ">Event name</p>
+                <p className="lg:text-xl font-bold flex items-center ">Event Date</p>
+                {/* <p className="lg:text-xl font-bold flex items-center justify-center text-center">Edit</p>
+                <p className="lg:text-xl font-bold flex items-center justify-center text-center">Go to participant surveys</p>
+                <p className="lg:text-xl font-bold flex items-center justify-center text-center">Upload event documents</p>
+                <p className="lg:text-xl font-bold flex items-center justify-center text-center">Complete post-event survey</p> */}
             </div>
           {/* TABLE HEAD END */}
 
 
-        <div className="events-index-btn-container grid grid-cols-1 gap-2  p-0">
+        <div className="events-index-btn-container grid grid-cols-1 gap-3 p-0 ">
           {sortedEventsByDate && sortedEventsByDate.map((event, index) => (
-            <section key={index} className={`existing-events-head-table px-1 py-3 ${index % 2 !== 0 ? "bg-white" : "border-b border-t"}`}>
-              <div className="flex items-start lg:text-lg justify-center">{event.programname}</div>
-              <div className="flex items-start lg:text-lg pr-2">{event.eventname}</div>
-              <div className="flex items-start lg:text-lg">{event.eventdate && new Date(event?.eventdate).toLocaleDateString('en-US',{year:'numeric',month:'numeric',day:'numeric'})}</div>
+            <section key={index} className={`existing-events-head-table px-7 py-7 rounded shadow`}>
+              <div className="flex items-center lg:text-xl font-bold ">{event.programname}</div>
+              <div className="flex items-center lg:text-xl font-bold ">{event.eventname}</div>
+              <div className="flex items-center lg:text-xl font-bold mr-2">{event.eventdate && new Date(event?.eventdate).toLocaleDateString('en-US',{year:'numeric',month:'numeric',day:'numeric'})}</div>
               <Link href={`events/${event.id}/edit`}>
-              <div className="cursor-pointer flex items-start lg:text-lg justify-center">
+              <div className="cursor-pointer flex items-center border shadow rounded text-center lg:text-xl p-4 font-bold justify-center">
 
                   Edit event
                   {/* <img src="/events/manage/edit.svg" alt="edit event icon"/> */}
@@ -41,22 +41,22 @@ const EventsIndex = ({events}) => {
 
               </Link>
               <Link href={`/events/${event.id}/participant-survey`}>
-                <div className="cursor-pointer flex items-start lg:text-lg justify-center">
-                  Go to participant surveys
+                <div className="cursor-pointer flex items-center border shadow rounded text-center lg:text-xl p-4 font-bold justify-center">
+                  Participant<br/>survey
                   {/* <img src="/events/manage/participant_survey.svg" alt="go to participant surveys icon"/> */}
                 </div>
               </Link>
               
               <Link href={`events/${event.id}/upload-event`}>
-                <div className="cursor-pointer flex items-start lg:text-lg justify-center">
-                Upload event documents
+                <div className="cursor-pointer flex items-center border shadow rounded text-center lg:text-xl p-4 font-bold justify-center">
+                Upload docs,<br/>photos, etc
                   {/* <img src="/events/manage/Upload_docs.svg" alt="upload event documents icon"/> */}
                 </div>
               </Link>
               
               <Link href={`events/${event.id}/post-event-survey`}>
-                <div className="cursor-pointer flex items-start lg:text-lg justify-center">
-                Complete post-event survey
+                <div className="cursor-pointer flex items-center border shadow rounded text-center lg:text-xl p-4 font-bold justify-center">
+                Complete<br/> post-event survey
                   {/* <img src="/events/manage/complete_report.svg" alt="complete report icon"/> */}
                 </div>
               </Link>
