99a7a3a5f99018f7876a0cfcb9aef6713819a274
Alexei Garban <garban.valdeon@gmail.com>
2022-09-28 13:58:04 +0200

    fix merge conflict

--- components/ReportProblem.js
+++ components/ReportProblem.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 const ReportProblem = () => {
   return (
-<<<<<<< HEAD
+
     <div className='container mx-auto py-10 w-full cursor-pointer  underline text-center'>
         <a href="https://airtable.com/shr8tmgc0QnAvziz5" target="_blank" className='text-lg'>Report an issue or make a request</a>
-=======
-    <div className='container mx-auto py-10 w-full  cursor-pointer  underline text-right'>
-        <a href="https://airtable.com/shr8tmgc0QnAvziz5" target="_blank" className='text-lg'>Would you like to report an issue?</a>
->>>>>>> 9f4d31cee29db1596a44d5b1738930e01f3e172c
+
     </div>
   )
 }
