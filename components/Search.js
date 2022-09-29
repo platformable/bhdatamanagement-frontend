0c27f0e3243f61002f37406a7e542a23bbc41893
leoncan122 <leon.can122@gmail.com>
2022-09-29 15:31:56 +0200

    Update text bold

--- components/Search.js
+++ components/Search.js
@@ -21,7 +21,7 @@ export default function Search({searchFunction}) {
     id="default-search" class="block p-4 w-full text-sm border-black-md text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
 
         <div class="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
         
     </div>                   {/*  <button className="px-2 py-2 m-1 text-white bg-black-50  rounded">
--- components/airsForm/AirsDemographics.js
+++ components/airsForm/AirsDemographics.js
@@ -7,7 +7,7 @@ const AirsDemographics = ({event}) => {
     const demographiscknown = event.hivtestedtotal+event.stitestedtotal+event.hepctestedtotal+event.covidtestedtotal
     return (
         <div className="border-black  mx-5 ">
-            <p className=" text-center my-2 text-xs">* Demographics of participants</p>
+            <p className="font-bold  text-center my-2 text-xs">* Demographics of participants</p>
 
    
 
@@ -16,72 +16,72 @@ const AirsDemographics = ({event}) => {
                     
 
                     <div id="gender" className="leading-5 py-2 px-3">
-                    <p className="text-xxs">* GENDER</p>
+                    <p className="font-bold text-xxs">* GENDER</p>
 
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">WOMAN/GIRL:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">TRANSGENDER WOMAN/GIRL: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">MAN/BOY: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">TRANSGENDER MAN/BOY: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">NON-BINARY PERSON: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">GENDER NON-CONFORMING PERSON: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">NO SURE/QUESTIONING: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">GENDER NOT LISTED: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">CHOSE NOT TO RESPOND: </p><p className="underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">WOMAN/GIRL:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">TRANSGENDER WOMAN/GIRL: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">MAN/BOY: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">TRANSGENDER MAN/BOY: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">NON-BINARY PERSON: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">GENDER NON-CONFORMING PERSON: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">NO SURE/QUESTIONING: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">GENDER NOT LISTED: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">CHOSE NOT TO RESPOND: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
                     
                     </div>
                     <div id="gender" className="leading-5 py-2 px-3">
-                    <p className="grid text-xxs">* AGE GROUPS</p>
+                    <p className="font-bold grid text-xxs">* AGE GROUPS</p>
 
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">WOMAN/GIRL:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">TRANSGENDER WOMAN/GIRL: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">MAN/BOY: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">TRANSGENDER MAN/BOY: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">NON-BINARY PERSON: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5 "><p className="text-xxs w-60">GENDER NON-CONFORMING PERSON: </p><p className="underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">WOMAN/GIRL:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">TRANSGENDER WOMAN/GIRL: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">MAN/BOY: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">TRANSGENDER MAN/BOY: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">NON-BINARY PERSON: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">GENDER NON-CONFORMING PERSON: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
                     </div>
                 </div>
                 <div className="divide-y divide-black">
                     <div className='py-2 px-3  leading-5'>
-                    <p className="grid text-xxs">* RACE / ETHNICITY</p>
+                    <p className="font-bold grid text-xxs">* RACE / ETHNICITY</p>
 
-                        <div className="flex gap-x-5"><p className="text-xxs w-60">HISPANIC::</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">(NH) WHITE: </p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">(NH) BLACK OR AFRICAN AMERICAN:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">(NH) ASIAN:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">(NH) NATIVE HAWAIIAN OR PACIFIC ISLANDER:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">(NH) AMERICAN INDIAN OR ALASKAN NATIVE:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">MORE THAN ONE RACE / ETHNICITY:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">OTHER:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                        <div className="flex gap-x-5 "><p className="text-xxs w-60">UNKNOWN / UNREPORTED:</p><p className="underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5"><p className="font-bold text-xxs w-60">HISPANIC::</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">(NH) WHITE: </p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">(NH) BLACK OR AFRICAN AMERICAN:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">(NH) ASIAN:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">(NH) NATIVE HAWAIIAN OR PACIFIC ISLANDER:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">(NH) AMERICAN INDIAN OR ALASKAN NATIVE:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">MORE THAN ONE RACE / ETHNICITY:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">OTHER:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                        <div className="flex gap-x-5 "><p className="font-bold text-xxs w-60">UNKNOWN / UNREPORTED:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
 
                     </div>
                     <div className="leading-5 py-2 px-3">
-                    <p className="text-xxs">* TOTAL PARTICIPANTS</p>
-                    <p className="text-xxs">Demographics known: {demographiscknown} </p>
-                    <p className="text-xxs">Demographics unknown: {demographiscknown} </p>
+                    <p className="font-bold text-xxs">* TOTAL PARTICIPANTS</p>
+                    <p className="font-bold text-xxs">Demographics known: {demographiscknown} </p>
+                    <p className="font-bold text-xxs">Demographics unknown: {demographiscknown} </p>
                     </div>
                 </div>
                 
             </div>
-            <p className="border-t border-t-black text-xxs py-1 px-3">* PRIMARY RISK</p>
+            <p className="font-bold border-t border-t-black text-xxs py-1 px-3">* PRIMARY RISK</p>
         <div className="grid grid-cols-3 gap-x-5">
 
             <div className="py-2 px-3"> 
                 
-                <div className="flex gap-x-5"><p className="w-20 text-xxs">IDU:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5"><p className="w-20 text-xxs">MSM:</p><p className="underline text-xxs">{demographiscknown} </p></div>
+                <div className="flex gap-x-5"><p className="font-bold w-20 text-xxs">IDU:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5"><p className="font-bold w-20 text-xxs">MSM:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
             </div>
 
             <div className="py-2 px-3"> 
            
-                <div className="flex gap-x-5"><p className="w-60 text-xxs">MSM / IDU:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5"><p className="w-60 text-xxs">SEX INVOLVING TRANSGENDER:</p><p className="underline text-xxs">{demographiscknown} </p></div>
+                <div className="flex gap-x-5"><p className="font-bold w-60 text-xxs">MSM / IDU:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5"><p className="font-bold w-60 text-xxs">SEX INVOLVING TRANSGENDER:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
             </div>
 
             <div className="py-2 px-3"> 
                 
-                <div className="flex gap-x-5"><p className="w-60 text-xxs">HETEROSEXUAL CONTACT:</p><p className="underline text-xxs">{demographiscknown} </p></div>
-                    <div className="flex gap-x-5"><p className="w-60 text-xxs">OTHER / RISK NOT INDENTIFIED:</p><p className="underline text-xxs">{demographiscknown} </p></div>
+                <div className="flex gap-x-5"><p className="font-bold w-60 text-xxs">HETEROSEXUAL CONTACT:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
+                    <div className="flex gap-x-5"><p className="font-bold w-60 text-xxs">OTHER / RISK NOT INDENTIFIED:</p><p className="font-bold underline text-xxs">{demographiscknown} </p></div>
             </div>
 
         </div>
