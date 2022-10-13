30177cd96b3e81a4f8ad0085a184249554f1d2ea
leoncan122 <leon.can122@gmail.com>
2022-10-13 18:30:04 +0200

    update

--- pages/events/index.js
+++ pages/events/index.js
@@ -93,19 +93,7 @@ const EventsIndex = ({ events }) => {
     return icsFile;
   }
 
-  const crearFecha2 = (event) => {
-    let options = { 
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      };
-    options.timeZone = 'UTC';
-    const date = new Date(event?.eventdate);
-    const result = new Intl.DateTimeFormat('en-US', options).format(date);
-    console.log("result", result)
-    const splitted = result.split("/")
-    return `${splitted[2]}-${splitted[0]}-${splitted[1]}`
-  };
+  
   const searchFunction = (word) => {
     setSearchWord(word);
     dispatch(searchEventByName({ word }));
