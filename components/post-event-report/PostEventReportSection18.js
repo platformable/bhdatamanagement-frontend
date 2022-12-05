46f41679d2cf773dbd074c5cd9c517e2a9c880f4
leoncan122 <leon.can122@gmail.com>
2022-12-05 20:31:38 +0100

    Update Event highlights

--- components/post-event-report/PostEventReportSection18.js
+++ components/post-event-report/PostEventReportSection18.js
@@ -7,7 +7,7 @@ const PostEventReportSection18 = ({eventForm,setEventForm}) => {
        <h2 className="mb-7 font-black">Please describe the event: <br/>Describe the event including highlights, partners, event objectives and outcomes, etc</h2>
 
       <textarea
-        className="p-4 block w-full text-lg h-52 bg-white break-all border-black rounded-md overflow-hidden"
+        className="p-4 block w-full text-lg h-52 whitespace-pre-wrap bg-white break-all border-black rounded-md overflow-hidden"
         role="textbox"
         name="eventHighlights"
         placeholder=""
