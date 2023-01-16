8c90a6c8001eda053f035d5539c2c645a2231598
leoncan122 <leon.can122@gmail.com>
2023-01-16 20:34:49 +0100

    Add cbt participant survey's sections

--- components/oef-cbt-participant-survey/Fbo.js
+++ components/oef-cbt-participant-survey/Fbo.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
 
-const Fbo = () => {
-    return (
-        <div>
-            Fbo
+const Fbo = ({ fbos, surveyForm, setSurveyForm }) => {
+  console.log();
+  const handleForm = (e) => {
+    setSurveyForm((prev) => ({ ...prev, fbo: e.target.value }));
+  };
+  return (
+    <div className="question-body">
+      <h2 className="font-black">Which FBO are you from?</h2>
+      <div className="grid grid-cols-2 gap-5">
+        <div className="grid gap-5">
+        {fbos?.slice(0, Math.round(fbos.length / 2)).sort((a, b) => a.namefbo.localeCompare(b.namefbo, 'en')).map((fbo, index) => (
+          <label className="flex gap-5 items-center">
+            <input
+              type="radio"
+              className=""
+            //   defaultChecked={}
+              value={fbo.namefbo}
+              onChange={handleForm}
+              name="fbo"
+            />
+            <p className="">{fbo.namefbo}</p>
+          </label>
+        ))}
         </div>
-    );
-}
+        <div className="grid gap-5">
+        {fbos?.slice(Math.round(fbos.length / 2)).sort((a, b) => a.namefbo.localeCompare(b.namefbo, 'en')).map((fbo, index) => (
+          <label className="flex gap-5 items-center">
+            <input
+              type="radio"
+              className=""
+            //   defaultChecked={}
+              value={fbo.namefbo}
+              onChange={handleForm}
+              name="fbo"
+            />
+            <p className="">{fbo.namefbo}</p>
+          </label>
+        ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Fbo;
+
--- components/oef-cbt-participant-survey/FboPosition.js
+++ components/oef-cbt-participant-survey/FboPosition.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React from "react";
 
-const FboPosition = () => {
-    return (
-        <div>
-           FboPosition 
+const FboPosition = ({ surveyForm, setSurveyForm }) => {
+  console.log();
+  const handleForm = (e) => {
+    setSurveyForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const fbosPosition = [
+    "Leader (Pastor, Imam, Deacon)",
+    "Coordinator",
+    "Youth Leader",
+    "Representative",
+    "Community Member",
+  ]
+  return (
+    <div className="question-body">
+      <h2 className="font-black">What is your position/title in the FBO?</h2>
+      <div className="grid grid-cols-2 gap-5">
+        <div className="grid gap-5">
+        {fbosPosition?.slice(0, Math.round(fbosPosition.length / 2)).sort((a, b) => a.localeCompare(b, 'en')).map((fbo, index) => (
+          <label className="flex gap-5 items-center" key={index}>
+            <input
+              type="radio"
+              className=""
+            //   defaultChecked={}
+              value={fbo}
+              onChange={handleForm}
+              name="fboPosition"
+            />
+            <p className="">{fbo}</p>
+          </label>
+        ))}
         </div>
-    );
-}
+        <div className="grid gap-5" >
+        {fbosPosition?.slice(Math.round(fbosPosition.length / 2)).sort((a, b) => a.localeCompare(b, 'en')).map((fbo, index) => (
+          <label className="flex gap-5 items-center" key={index}>
+            <input
+              type="radio"
+              className=""
+            //   defaultChecked={}
+              value={fbo}
+              onChange={handleForm}
+              name="fboPosition"
+            />
+            <p className="">{fbo}</p>
+          </label>
+        ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default FboPosition;
+
