4a868575416fb1c17f1963730757a5f012b84780
leoncan122 <leon.can122@gmail.com>
2023-01-17 23:26:33 +0100

    update demographics

--- components/oef-cbt-participant-survey/Gender.js
+++ components/oef-cbt-participant-survey/Gender.js
@@ -22,20 +22,10 @@ const Gender = ({surveyForm, setSurveyForm}) => {
       ))}
       </div>
       <div className='grid gap-5'>
-      {gender?.slice(8 / 2).map(option => (
+      {gender?.slice(8 / 2).map(option => option.value === 'Other gender identity' ? (
         <label className="flex gap-x-5 items-center">
         <input type="radio" className=" " value={option.value} id={option.id} onChange={handleForm} name="participantGender" />
         <p className="">{option.value}</p>
-      </label>
-      ))}
-      <label className="flex gap-x-5 items-center">
-          <input type="radio" className="" value="Decline to answer" id={8} onChange={handleForm} name="participantGender" />
-          <p className="">Decline to answer </p>
-        </label>
-
-        <label className="flex gap-x-5 items-center">   
-        <input type="radio" className="" value={'Other gender identity'} id={'Other'} onChange={handleForm} name="participantGender" />
-        <p className="">{'Other'}</p>
         <input
           type="text"
           placeholder="Please specify"
@@ -48,6 +38,19 @@ const Gender = ({surveyForm, setSurveyForm}) => {
           className="border-black rounded p-4 self-start p-1 w-full text-lg md:w-134"
         />
       </label>
+      ) : (
+        <label className="flex gap-x-5 items-center">
+        <input type="radio" className=" " value={option.value} id={option.id} onChange={handleForm} name="participantGender" />
+        <p className="">{option.value}</p>
+        
+      </label>
+      ))}
+      <label className="flex gap-x-5 items-center">
+          <input type="radio" className="" value="Decline to answer" id={8} onChange={handleForm} name="participantGender" />
+          <p className="">Decline to answer </p>
+        </label>
+
+       
       </div> 
            
       </div>
--- components/oef-cbt-participant-survey/Race.js
+++ components/oef-cbt-participant-survey/Race.js
@@ -1,43 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import { race } from '../../utils/sharedData';
+import React, { useState, useEffect } from "react";
+import { race } from "../../utils/sharedData";
 
-const Race = ({surveyForm, setSurveyForm}) => {
-  
-  const [data,setData]=useState([])
-  const [dataId,setDataId]=useState([])
+const Race = ({ surveyForm, setSurveyForm }) => {
+  const [data, setData] = useState([]);
+  const [dataId, setDataId] = useState([]);
 
+  const handleForm = (e) => {
+    const isValueOnData = data?.includes(e.target.value);
+    const isIdOnDataId = dataId?.includes(Number(e.target.id));
 
-  const handleForm=(e)=>{
-    const isValueOnData=data?.includes(e.target.value)
-    const isIdOnDataId=dataId?.includes(Number(e.target.id))
+    const filteredData = data.filter(
+      (oldValues) => oldValues != e.target.value
+    );
+    const filteredDataIds = dataId.filter(
+      (oldValues) => oldValues != e.target.id
+    );
 
-   
-    const filteredData=data.filter(oldValues=> oldValues != e.target.value) 
-    const filteredDataIds=dataId.filter(oldValues=> oldValues != e.target.id) 
+    isValueOnData
+      ? setData(filteredData)
+      : setData((previous) => [...previous, e.target.value]);
 
-    
-    isValueOnData?
-    setData(filteredData)  :
-    setData((previous)=>([
-      ...previous,e.target.value
-    ]))
+    isIdOnDataId
+      ? setDataId(filteredDataIds)
+      : setDataId((previous) => [...previous, Number(e.target.id)]);
+  };
 
-    isIdOnDataId ? 
-    setDataId(filteredDataIds):
-    setDataId((previous)=>([
-      ...previous,Number(e.target.id)
-    ]))    
-
-
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     setSurveyForm((previous) => ({
       ...previous,
       participantRace: data,
-      raceID: dataId
-    }))
-  },[data])
+      raceID: dataId,
+    }));
+  }, [data]);
   return (
     <div className="px-7">
       <h2 className="font-black">
@@ -46,48 +40,64 @@ const Race = ({surveyForm, setSurveyForm}) => {
       </h2>
       <p>Select all that apply</p>
       <div className="mt-7 grid grid-cols-2 ">
-      <div className='grid gap-5'>
-      {race.slice(0, Math.round(race.length /2)).map(option => (
-        <label className="flex gap-x-5 items-center">
-        <input type="checkbox" className="" value={option.value} id={option.id} onChange={handleForm} name="participantRace" />
-        <p className="">{option.value}</p>
-      </label>
-      ))}
-    
-      </div>
-      <div className='grid gap-5'>
-      {race.slice(Math.round(race.length /2)).map(option => (
-        <label className="flex gap-x-5 items-center">
-        <input type="checkbox" className="" value={option.value} id={option.id} onChange={handleForm} name="participantRace" />
-        <p className="">{option.value}</p>
-      </label>
-      
-      ))}
-
-      <label className="flex gap-x-5 items-center">   
-        <input type="checkbox" className="" value={'Other'} id={'Other'} onChange={handleForm} name="participantRace" />
-        <p className="">{'Other'}</p>
-        <input
-          type="text"
-          placeholder="Please specify"
-          onChange={(e) =>
-            setSurveyForm({
-              ...surveyForm,
-              participantRaceOther: e.target.value,
-            })
-          }
-          className="border-black rounded p-4 self-start p-1 w-full text-lg md:w-134"
-        />
-      </label>
-      
-      </div>
-      
-
-      
-       
+        <div className="grid gap-5">
+          {race.slice(0, Math.round(race.length / 2)).map((option) => (
+            <label className="flex gap-x-5 items-center">
+              <input
+                type="checkbox"
+                className=""
+                value={option.value}
+                id={option.id}
+                onChange={handleForm}
+                name="participantRace"
+              />
+              <p className="">{option.value}</p>
+            </label>
+          ))}
+        </div>
+        <div className="grid gap-5">
+          {race.slice(Math.round(race.length / 2)).map((option) =>
+            option.value !== "Some other race or origin" ? (
+              <label className="flex gap-x-5 items-center">
+                <input
+                  type="checkbox"
+                  className=""
+                  value={option.value}
+                  id={option.id}
+                  onChange={handleForm}
+                  name="participantRace"
+                />
+                <p className="">{option.value}</p>
+              </label>
+            ) : (
+              <label className="flex gap-x-5 items-center">
+                <input
+                  type="checkbox"
+                  className=""
+                  value={option.value}
+                  id={option.id}
+                  onChange={handleForm}
+                  name="participantRace"
+                />
+                <p className="">{option.value}</p>
+                <input
+                  type="text"
+                  placeholder="Please specify"
+                  onChange={(e) =>
+                    setSurveyForm({
+                      ...surveyForm,
+                      participantRaceOther: e.target.value,
+                    })
+                  }
+                  className="border-black rounded p-4 self-start p-1 w-full text-lg md:w-134"
+                />
+              </label>
+            )
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Race;
