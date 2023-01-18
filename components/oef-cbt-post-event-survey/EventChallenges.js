34ea720b8f1f11f50c5183b82ca3be07945d095e
leoncan122 <leon.can122@gmail.com>
2023-01-18 14:18:08 +0100

    add css new colors, add post event questions

--- components/oef-cbt-post-event-survey/EventChallenges.js
+++ components/oef-cbt-post-event-survey/EventChallenges.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const EventChallenges = () => {
+const EventChallenges = ({ eventForm, setEventForm }) => {
     return (
-        <div>
-            EventChallenges
-        </div>
+        <div className='question-body'>
+        <h2 className="mb-7 font-black">Were there any other challenges in running the workshop?</h2>
+        <label >
+          <textarea
+            className="p-4 block w-full text-lg h-52 bg-white break-all border-black rounded-md overflow-hidden"
+            role="textbox"
+            name="eventChallenges"
+            placeholder=""
+            onChange={(e) =>
+              setEventForm((previous) => ({
+                ...previous,
+                [e.target.name]: e.target.value,
+              }))
+            }
+            defaultValue={eventForm?.eventChallenges }
+          />
+        </label>
+      </div>
     );
 }
 
--- components/oef-cbt-post-event-survey/ImproveEngagement.js
+++ components/oef-cbt-post-event-survey/ImproveEngagement.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const ImproveEngagement = () => {
+const ImproveEngagement = ({ eventForm, setEventForm }) => {
     return (
-        <div>
-            ImproveEngagement
-        </div>
+        <div className='question-body'>
+        <h2 className="mb-7 font-black">How could this be improved next time?</h2>
+        <label >
+          <textarea
+            className="p-4 block w-full text-lg h-52 bg-white break-all border-black rounded-md overflow-hidden"
+            role="textbox"
+            name="improveEngagement"
+            placeholder=""
+            onChange={(e) =>
+              setEventForm((previous) => ({
+                ...previous,
+                [e.target.name]: e.target.value,
+              }))
+            }
+            defaultValue={eventForm?.improveEngagement }
+          />
+        </label>
+      </div>
     );
 }
 
