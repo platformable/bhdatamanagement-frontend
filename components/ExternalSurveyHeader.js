4dc684856f82a04cea8d823c262037298857a5cc
leoncan122 <leon.can122@gmail.com>
2023-02-08 15:24:03 +0100

    Edit dbt post event survey

--- components/ExternalSurveyHeader.js
+++ components/ExternalSurveyHeader.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ExternalSurveyHeader = ({pageTitle}) => {
     return (
-        <div className="h-88 py-10 container mx-auto mt-3 flex mb-10 flex-col items-center rounded-lg border-black">
+        <div className="h-88 py-10 container mx-auto mt-3 flex mb-10 px-7 md:px-0 flex-col items-center rounded-lg border-black">
         <h1 className="text-center font-black">{pageTitle}</h1>
-        <div className="flex items-center gap-5 mt-5">
+        <div className="flex flex-col md:flex-row items-center  gap-5 mt-5">
           <h2 className="pt-2">In partnership with</h2>
           <img
             src="/main/BH_logo.svg"
