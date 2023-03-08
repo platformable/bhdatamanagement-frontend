7cc493b46ffd8e74ec61d4811d3c0293b0e098ed
Alexei Garban <garban.valdeon@gmail.com>
2023-03-08 16:24:00 +0100

    fix csv

--- components/csv-reports/CSVHIVOutreachParticipantSignInSheet.js
+++ components/csv-reports/CSVHIVOutreachParticipantSignInSheet.js
@@ -14,7 +14,7 @@ const orderDataset = (data, headers) => {
     if (typeof data[header] === 'object' ) {
       `${data[header].join(', ')}`
     }
-    return `${data[header.toLowerCase()]}`.replace(/$(\r|\n)(?=.)/gm, " ");
+    return `${data[header.toLowerCase()]}`.replace(/$(\r|\n)(?=.)/gm, " ").replace(/"/g,'');
   })
   return results;
 };
