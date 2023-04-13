a807fda79a767756521d41de942cd4af2dbed7f4
leoncan122 <leon.can122@gmail.com>
2023-04-13 09:32:28 +0200

    Add new Input for address

--- components/events/LocationAddress.js
+++ components/events/LocationAddress.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import InputValidationAddress from '../InputValidationAddress'
+import React from "react";
+import InputValidationAddress from "../InputValidationAddress";
 
-const LocationAddress = ({eventForm,setEventForm}) => {
-
+const LocationAddress = ({ eventForm, setEventForm }) => {
   return (
     <div className=" rounded">
-    <h2 className="font-black mb-7">
-      What was the event location address
-      <span className='text-xl text-red-500 ml-2'>*required</span>
-    </h2>
-    <label>
-    {/* <input type="text" 
+      <h2 className="font-black mb-7">
+        What was the event location address
+        <span className="text-xl text-red-500 ml-2">*required</span>
+      </h2>
+      <label>
+        {/* <input type="text" 
     name="locationAddress" 
     className='border-black rounded mt-5  text-lg w-134'
     onChange={(e)=>{
@@ -22,11 +21,27 @@ const LocationAddress = ({eventForm,setEventForm}) => {
     defaultValue={eventForm.locationAddress}
     
 /> */}
-    <InputValidationAddress setForm={setEventForm} name={'locationAddress'} defaultValue={eventForm.locationAddress}/>
-    </label>
-    
-  </div>
-  )
-}
+        {eventForm?.onlineInPersonEventType === "Online" ? (
+          <input
+            type="text"
+            className="md:w-96"
+            onClick={(e) =>
+              setEventForm((prev) => ({
+                ...prev,
+                locationAddress: e.target.value,
+              }))
+            }
+          />
+        ) : (
+          <InputValidationAddress
+            setForm={setEventForm}
+            name={"locationAddress"}
+            defaultValue={eventForm.locationAddress}
+          />
+        )}
+      </label>
+    </div>
+  );
+};
 
-export default LocationAddress
+export default LocationAddress;
